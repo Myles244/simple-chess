283146e201eeb53a820597c7774baca2b951b2c7
Myles244 <43722484+Myles244@users.noreply.github.com>
2024-11-16 02:17:04 +0000

    can now select a square

--- board.py
+++ board.py
@@ -1,14 +1,19 @@
 import pygame
+import numpy as np
 
 BOARD_SIZE=800
 SQUARE_SIZE=BOARD_SIZE/8
 
 WHITE_SQUARE_COLOUR=(255, 220, 180)
 BLACK_SQUARE_COLOUR=(100, 50, 20)
+HIGHTLIGHTED_SQUARE_COLOUR=(250, 220, 70)
 
 #function to draw a blanc chess board
-def draw_chess_board(screen):
+def draw_chess_board(screen,highlighted_square):
 	for row in range(8):
 		for col in range(8):
 			square_colour = WHITE_SQUARE_COLOUR if (row+col)%2==0 else BLACK_SQUARE_COLOUR
+			if row==highlighted_square[1] and col == highlighted_square[0]:
+				square_colour=HIGHTLIGHTED_SQUARE_COLOUR
+
 			pygame.draw.rect(screen,square_colour,(col*SQUARE_SIZE,row*SQUARE_SIZE,SQUARE_SIZE,SQUARE_SIZE))
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # Example file showing a basic pygame "game loop"
 import pygame
+import numpy as np
+
 from board import *
 from pieces import *
 from logic import *
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((BOARD_SIZE, BOARD_SIZE))
@@ -12,6 +15,8 @@ pygame.display.set_caption("Simple Chess")
 
 game=ChessGame()
 
+selected_square=(8,8)
+
 while running:
 	
 	# poll for events
@@ -19,9 +24,11 @@ while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			selected_square=np.array(pygame.mouse.get_pos())//SQUARE_SIZE
 
 	# fill the screen with a color to wipe away anything from last frame
-	draw_chess_board(screen)
+	draw_chess_board(screen,selected_square)
 
 	# RENDER YOUR GAME HERE
 	game.draw(screen)
