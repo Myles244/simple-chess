81a4758a398db83d6fd53b4aa943995f08986fc2
Myles244 <43722484+Myles244@users.noreply.github.com>
2024-11-16 23:47:07 +0000

    made more moves

--- logic.py
+++ logic.py
@@ -4,6 +4,9 @@ import numpy as np
 
 class ChessGame:
 	def __init__(self):
+		
+		#variable to keep track of checks, since you can only be in check if its your go we dont have to record who is in check
+		self.check=False
 
 		#a 2d array of the piece at each square in the board
 		self.piece_map=[
@@ -23,34 +26,34 @@ class ChessGame:
 
 		#the pawns
 		for i in range(8):
-			self.white_pieces.append(WhitePawn((6,i)))
-			self.black_pieces.append(BlackPawn((1,i)))
+			self.white_pieces.append(Pawn((6,i),True))
+			self.black_pieces.append(Pawn((1,i),False))
 
 		#the kings
-		self.white_pieces.append(WhiteKing((7,3)))
-		self.black_pieces.append(BlackKing((0,3)))
+		self.white_pieces.append(King((7,3),True))
+		self.black_pieces.append(King((0,3),False))
 
 		#the queens
-		self.white_pieces.append(WhiteQueen((7,4)))
-		self.black_pieces.append(BlackQueen((0,4)))
+		self.white_pieces.append(Queen((7,4),True))
+		self.black_pieces.append(Queen((0,4),False))
 
 		#the bishops
-		self.white_pieces.append(WhiteBishop((7,2)))
-		self.white_pieces.append(WhiteBishop((7,5)))
-		self.black_pieces.append(BlackBishop((0,2)))
-		self.black_pieces.append(BlackBishop((0,5)))
+		self.white_pieces.append(Bishop((7,2),True))
+		self.white_pieces.append(Bishop((7,5),True))
+		self.black_pieces.append(Bishop((0,2),False))
+		self.black_pieces.append(Bishop((0,5),False))
 
 		#the knights
-		self.white_pieces.append(WhiteKnight((7,1)))
-		self.white_pieces.append(WhiteKnight((7,6)))
-		self.black_pieces.append(BlackKnight((0,1)))
-		self.black_pieces.append(BlackKnight((0,6)))
+		self.white_pieces.append(Knight((7,1),True))
+		self.white_pieces.append(Knight((7,6),True))
+		self.black_pieces.append(Knight((0,1),False))
+		self.black_pieces.append(Knight((0,6),False))
 
 		#the rooks
-		self.white_pieces.append(WhiteRook((7,0)))
-		self.white_pieces.append(WhiteRook((7,7)))
-		self.black_pieces.append(BlackRook((0,0)))
-		self.black_pieces.append(BlackRook((0,7)))
+		self.white_pieces.append(Rook((7,0),True))
+		self.white_pieces.append(Rook((7,7),True))
+		self.black_pieces.append(Rook((0,0),False))
+		self.black_pieces.append(Rook((0,7),False))
 
 	#draw the pieces on the creen
 	def draw(self,screen):
@@ -61,6 +64,8 @@ class ChessGame:
 
 	#return a list of possible moves for the piece at a give square including itslef
 	def get_possible_moves(self,square,white_to_move):
+		if self.check:
+			return []
 		if white_to_move:
 			for piece in self.white_pieces:
 				if np.all(piece.position == square):
@@ -73,7 +78,7 @@ class ChessGame:
 	
 	#move the piece at the start position if it exists to the end position
 	def move(self, white_to_move,start_position, end_position):
-		
+
 		#remove piece if their is one being taken
 		if self.piece_map[end_position[0]][end_position[1]]!=0:
 			if white_to_move:
--- pieces.py
+++ pieces.py
@@ -3,9 +3,10 @@ from board import *
 import numpy as np
 
 class ChessPiece:
-	def __init__(self,image_file_name, position):
+	def __init__(self,image_file_name, position, is_white):
 		self.image = pygame.image.load(image_file_name)
 		self.position=np.array(position)
+		self.is_white=is_white
 
 	def draw(self,screen):
 		screen.blit(self.image,(self.position[1]*SQUARE_SIZE,self.position[0]*SQUARE_SIZE))
@@ -17,67 +18,70 @@ class ChessPiece:
 		self.position=new_position
 	
 
-class WhitePawn(ChessPiece):
-	def __init__(self,position):
-		super().__init__("images/white_pawn.svg",position)
+class Pawn(ChessPiece):
+	def __init__(self,position,is_white):
+		if is_white:
+			super().__init__("images/white_pawn.svg",position,is_white)
+		else:
+			super().__init__("images/black_pawn.svg",position,is_white)
+		self.first_move=True
 
 	def possible_moves(self,piece_map):
 		moves=[]
+
+		#move forward two if the space is empty and its my first move
+		if self.first_move:
+			if piece_map[self.position[0]-2][self.position[1]]==0:
+				moves+=[(self.position[0]-2,self.position[1])]
 
 		#moveforward if the space is empty
 		if piece_map[self.position[0]-1][self.position[1]]==0:
 			moves+=[(self.position[0]-1,self.position[1])]
 
-		#taek diagonally if the space is full
+		#take diagonally if the space is full
 		if piece_map[self.position[0]-1][self.position[1]-1]<0:
 			moves+=[(self.position[0]-1,self.position[1]-1)]
 		if piece_map[self.position[0]-1][self.position[1]+1]<0:
 			moves+=[(self.position[0]-1,self.position[1]+1)]
 		return moves
+	
+	def move_to(self, new_position):
+		self.first_move=False
+		return super().move_to(new_position)
 
-class BlackPawn(ChessPiece):
-	def __init__(self,position):
-		super().__init__("images/black_pawn.svg",position)
+class King(ChessPiece):
+	def __init__(self,position,is_white):
+		if is_white:
+			super().__init__("images/white_king.svg",position,is_white)
+		else:
+			super().__init__("images/black_king.svg",position,is_white)
 
-class WhiteKing(ChessPiece):
-	def __init__(self,position):
-		super().__init__("images/white_king.svg",position)
+class Queen(ChessPiece):
+	def __init__(self,position,is_white):
+		if is_white:
+			super().__init__("images/white_queen.svg",position,is_white)
+		else:
+			super().__init__("images/black_queen.svg",position,is_white)
 
-class BlackKing(ChessPiece):
-	def __init__(self,position):
-		super().__init__("images/black_king.svg",position)
+class Bishop(ChessPiece):
+	def __init__(self,position,is_white):
+		if is_white:
+			super().__init__("images/white_bishop.svg",position,is_white)
+		else:
+			super().__init__("images/black_bishop.svg",position,is_white)
 
-class WhiteQueen(ChessPiece):
-	def __init__(self,position):
-		super().__init__("images/white_queen.svg",position)
+class Knight(ChessPiece):
+	def __init__(self,position,is_white):
+		if is_white:
+			super().__init__("images/white_knight.svg",position,is_white)
+		else:
+			super().__init__("images/black_knight.svg",position,is_white)
 
-class BlackQueen(ChessPiece):
-	def __init__(self,position):
-		super().__init__("images/black_queen.svg",position)
-
-class WhiteBishop(ChessPiece):
-	def __init__(self,position):
-		super().__init__("images/white_bishop.svg",position)
-
-class BlackBishop(ChessPiece):
-	def __init__(self,position):
-		super().__init__("images/black_bishop.svg",position)
-
-class WhiteKnight(ChessPiece):
-	def __init__(self,position):
-		super().__init__("images/white_knight.svg",position)
-
-class BlackKnight(ChessPiece):
-	def __init__(self,position):
-		super().__init__("images/black_knight.svg",position)
-
-class WhiteRook(ChessPiece):
-	def __init__(self,position):
-		super().__init__("images/white_rook.svg",position)
-
-class BlackRook(ChessPiece):
-	def __init__(self,position):
-		super().__init__("images/black_rook.svg",position)
-
+class Rook(ChessPiece):
+	def __init__(self,position,is_white):
+		if is_white:
+			super().__init__("images/white_rook.svg",position,is_white)
+		else:
+			super().__init__("images/black_rook.svg",position,is_white)
 
 	
